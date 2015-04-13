3dd608f75a1dd165a1370ed66d5ea0ebde0115ee
victor <victor@centauro>
2015-04-12 23:05:27 -0500

    Second commit

--- e-school-mx.sql
+++ e-school-mx.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.5.42, for linux2.6 (x86_64)
 --
--- Host: localhost    Database: villaeducativa
+-- Host: localhost    Database: e-school-mx
 -- ------------------------------------------------------
 -- Server version	5.5.42
 
